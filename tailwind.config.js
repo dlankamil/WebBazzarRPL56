105a845f46fcd045ad51c2aa3e0c69ece06f610d
Dlan18 <fkamil502@gmail.com>
2024-12-23 22:06:34 +0700

    Update path folder, install tailwind & alpine, custom css animation, dll

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./product/**/*.html",
+    "./assets/js/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        vt323: ['VT323', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
